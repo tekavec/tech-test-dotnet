0f66cc89b8dbb4f8c72ff4c2f544d024ffd89cd3
Tomaz Tekavec <tomaz.tekavec@gmail.com>
2025-09-28 17:40:40 +0100

    First unit test

---
+++ ClearBank.DeveloperTest.Tests/Services/PaymentServiceShould.cs
@@ -0,0 +1,19 @@
+﻿using ClearBank.DeveloperTest.Services;
+using ClearBank.DeveloperTest.Types;
+
+namespace ClearBank.DeveloperTest.Tests.Services
+{
+    public class PaymentServiceShould
+    {
+        [Fact]
+        public void Not_Make_Successful_Payment_If_Account_Does_Not_Exist()
+        {
+            var paymentService = new PaymentService();
+            var makePaymentRequest = new MakePaymentRequest { DebtorAccountNumber = "12345678", PaymentScheme = PaymentScheme.Bacs };
+
+            var result = paymentService.MakePayment(makePaymentRequest);
+
+            Assert.False(result.Success);
+        }
+    }
+}

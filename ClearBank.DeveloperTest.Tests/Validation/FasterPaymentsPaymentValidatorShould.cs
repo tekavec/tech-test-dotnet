26c9766d2772308263ffd90b14d0c5d088b785d9
Tomaz Tekavec <tomaz.tekavec@gmail.com>
2025-09-28 20:19:41 +0100

    Add FasterPaymentsPaymentValidator

---
+++ ClearBank.DeveloperTest.Tests/Validation/FasterPaymentsPaymentValidatorShould.cs
@@ -0,0 +1,105 @@
+﻿using ClearBank.DeveloperTest.Types;
+using ClearBank.DeveloperTest.Validation;
+
+namespace ClearBank.DeveloperTest.Tests.Validation
+{
+    public class FasterPaymentsPaymentValidatorShould
+    {
+        private readonly Account NonExistentAccount = null;
+        private const int SmallAmount = 1;
+        private const int LargeAmount = 100;
+
+        [Theory]
+        [InlineData(PaymentScheme.Bacs)]
+        [InlineData(PaymentScheme.Chaps)]
+        public void Not_Allow_Payment_If_Payment_Scheme_Is_Not_FasterPayments(PaymentScheme paymentScheme)
+        {
+            var bacsPaymentValidator = new FasterPaymentsPaymentValidator();
+            var paymentContext = GetFasterPaymentsPaymentContext(
+                paymentScheme, 
+                GetAccount(AllowedPaymentSchemes.FasterPayments, LargeAmount), 
+                SmallAmount);
+
+            MakePaymentResult result = bacsPaymentValidator.IsPaymentAllowed(paymentContext);
+
+            Assert.False(result.Success);
+        }
+
+        [Fact]
+        public void Not_Allow_Payment_If_No_Account_Provided()
+        {
+            var bacsPaymentValidator = new FasterPaymentsPaymentValidator();
+            var paymentContext = GetFasterPaymentsPaymentContext(
+                PaymentScheme.FasterPayments, 
+                NonExistentAccount, 
+                SmallAmount);
+
+            MakePaymentResult result = bacsPaymentValidator.IsPaymentAllowed(paymentContext);
+
+            Assert.False(result.Success);
+        }
+
+        [Fact]
+        public void Allow_Payment_If_Account_Allows_FasterPayments_Payment_Scheme_And_Account_Balance_Is_Adequate()
+        {
+            var fasterPaymentsPaymentValidator = new FasterPaymentsPaymentValidator();
+            var paymentContext = GetFasterPaymentsPaymentContext(
+                PaymentScheme.FasterPayments,
+                GetAccount(AllowedPaymentSchemes.FasterPayments, LargeAmount),
+                SmallAmount);
+
+            MakePaymentResult result = fasterPaymentsPaymentValidator.IsPaymentAllowed(paymentContext);
+
+            Assert.True(result.Success);
+        }
+
+        [Fact]
+        public void Not_Allow_Payment_If_Account_Allows_FasterPayments_But_Requested_Amount_Is_Greater_Than_Account_Balance()
+        {
+            var fasterPaymentsPaymentValidator = new FasterPaymentsPaymentValidator();
+            var paymentContext = GetFasterPaymentsPaymentContext(
+                PaymentScheme.FasterPayments,
+                GetAccount(AllowedPaymentSchemes.FasterPayments, SmallAmount),
+                LargeAmount);
+
+            MakePaymentResult result = fasterPaymentsPaymentValidator.IsPaymentAllowed(paymentContext);
+
+            Assert.False(result.Success);
+        }
+
+        [Theory]
+        [InlineData(AllowedPaymentSchemes.Bacs, PaymentScheme.Bacs, SmallAmount, LargeAmount)]
+        [InlineData(AllowedPaymentSchemes.Chaps, PaymentScheme.Chaps, SmallAmount, LargeAmount)]
+        [InlineData(AllowedPaymentSchemes.Bacs | AllowedPaymentSchemes.Chaps, PaymentScheme.Bacs, SmallAmount, LargeAmount)]
+        public void Not_Allow_Payment_If_Account_Does_Not_Allow_FasterPayments_Payment_Scheme(
+            AllowedPaymentSchemes allowedPaymentScheme,
+            PaymentScheme paymentScheme,
+            decimal amount,
+            decimal accountBalance)
+        {
+            var fasterPaymentsPaymentValidator = new FasterPaymentsPaymentValidator();
+            var paymentContext = GetFasterPaymentsPaymentContext(
+                paymentScheme,
+                GetAccount(allowedPaymentScheme, accountBalance),
+                amount);
+
+            MakePaymentResult result = fasterPaymentsPaymentValidator.IsPaymentAllowed(paymentContext);
+
+            Assert.False(result.Success);
+        }
+
+        private static Account GetAccount(AllowedPaymentSchemes allowedPaymentSchemes, decimal accountBalance) =>
+            new Account { AllowedPaymentSchemes = allowedPaymentSchemes, Balance = accountBalance };
+
+        private static MakePaymentRequest GetMakePaymentRequest(PaymentScheme paymentScheme, decimal amount) =>
+            new MakePaymentRequest { PaymentScheme = paymentScheme, Amount = amount };
+
+        private static PaymentContext GetFasterPaymentsPaymentContext(PaymentScheme paymentScheme, Account account, decimal amount) =>
+            new PaymentContext
+            {
+                Account = account,
+                MakePaymentRequest = GetMakePaymentRequest(paymentScheme, amount)
+            };
+
+    }
+}
---
+++ ClearBank.DeveloperTest/Validation/FasterPaymentsPaymentValidator.cs
@@ -0,0 +1,15 @@
+﻿using ClearBank.DeveloperTest.Types;
+
+namespace ClearBank.DeveloperTest.Validation
+{
+    public class FasterPaymentsPaymentValidator : IPaymentValidator
+    {
+        public MakePaymentResult IsPaymentAllowed(PaymentContext paymentContext) =>
+            paymentContext.MakePaymentRequest.PaymentScheme == PaymentScheme.FasterPayments
+            && paymentContext.Account != null
+            && paymentContext.Account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.FasterPayments)
+            && paymentContext.Account.Balance >= paymentContext.MakePaymentRequest.Amount
+                ? new MakePaymentResult { Success = true }
+                : new MakePaymentResult { Success = false };
+    }
+}
